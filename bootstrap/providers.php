29a55b1d4089c1e53ccaacc7b4b6759d3823a951
Cyrene <joedel.rf@gmail.com>
2026-08-24 00:41:38 +0800

    add

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    // App\Providers\TelescopeServiceProvider::class,
+    env('APP_ENV') == 'local' ? App\Providers\TelescopeServiceProvider::class : null,
     App\Providers\VoltServiceProvider::class,
 ];
